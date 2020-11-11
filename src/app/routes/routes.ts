e8e0378eb14dd9ce3c97df1ba0d89c108609784b
devfabiorm <fabio.evangelion.guitar@gmail.com>
2020-11-11 00:00:43 -0300

    Installing and setting SQLite3 and fetchinda data

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
 
+import bd from '../../config/database';
 import Template from '../views/livros/lista/lista.marko';
 
 const routes = Router();
+
+export interface Product {
+  id: number;
+  titulo: string;
+}
+
+const livros: Product[] = [];
 
 routes.get('/', function(request, response) {
   response.send(
@@ -18,7 +26,10 @@ routes.get('/', function(request, response) {
 });
   
 routes.get('/livros', function(request, response) {
-  response.marko(Template);
+  bd.all('SELECT * FROM livros', function(err, rows) {
+    response.marko(Template, { livros: rows });
+  });
+
 });
 
 export default routes;
